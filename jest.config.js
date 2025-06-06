a38899e7e94d6767387b41817cdb922140514fa6
Diego Lopes <dihgg@dihgg.com>
2025-06-06 20:19:29 -0300

    test(plushies): refactoring jest mocks

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,24 @@
 const { pathsToModuleNameMapper, createDefaultPreset } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
-const tsJestTransformCfg = createDefaultPreset().transform;
+const { transform } = createDefaultPreset();
 
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
   preset: "ts-jest",
-  // extensionsToTreatAsEsm:['.ts'],
+  rootDir: "src",
   setupFiles: [
     "./test/mock.ts"
   ],
-  transform: {
-    ...tsJestTransformCfg,
-  },
-  // modulePaths: ['./<rootDir>', 'node_modules'],
-  // moduleDirectories: ['./<rootDir>', 'node_modules'],
+  transform,
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>" }),
-  coverageDirectory: './coverage',
+  coverageDirectory: '../coverage',
   moduleFileExtensions: ['js', 'json', 'ts', 'd.ts', 'node'],
   testRegex: '.*\\.spec\\.ts$',
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
   coveragePathIgnorePatterns: ['index.(t|j)s'],
   transformIgnorePatterns: [
-    // '/node_modules/(?!<module-name>).+\\.js$',
+    "/node_modules/(?!<module-name>).+\\.js$",
   ],
 };
