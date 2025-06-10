ef9b24f4bcdecc970318f7cdf92bc27213240456
Diego Lopes <dihgg@dihgg.com>
2025-06-09 21:33:33 -0300

    test(mocks): refactoring mocks

--- src/__mocks__/@asledgehammer/pipewrench/index.ts
+++ src/__mocks__/@asledgehammer/pipewrench/index.ts
@@ -1,1 +1,6 @@
-export const getPlayer = jest.fn()
+import { mock } from "jest-mock-extended";
+import { IsoPlayer } from "@asledgehammer/pipewrench";
+
+export const getPlayer = jest.fn().mockImplementation(() => mock<IsoPlayer>())
+
+export const getText = jest.fn().mockImplementation((...args:string[]) => args.join());
--- src/__mocks__/src/client/components/Plushies/Plushie.ts
+++ src/__mocks__/src/client/components/Plushies/Plushie.ts
@@ -2,8 +2,10 @@ import {IsoPlayer} from "@asledgehammer/pipewrench";
 
 export class Plushie {
 	name = "";
+	player: IsoPlayer;
 	constructor(player: IsoPlayer, name: string) {
 		console.log('calling mocked plushie constructor');
+		this.player = player;
 		this.name = name;
 	}
 	update = jest.fn();
