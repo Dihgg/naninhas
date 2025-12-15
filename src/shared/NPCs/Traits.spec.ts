4fabc8205075f65f87be247f445211c8c3caccb2
Diego Lopes <dihgg@dihgg.com>
2025-12-15 16:28:22 -0300

    feat(traits): preventing build 42 on calling the Traits

--- src/shared/NPCs/Traits.spec.ts
+++ src/shared/NPCs/Traits.spec.ts
@@ -2,9 +2,20 @@ import { Traits } from "../components/Traits";
 jest.mock("../components/Traits");
 
 describe("TraitClass event registration", () => {
-	it("The TraitClass should be initialized", () => {
-		// triggers event registration
-		require("./Traits");
-		expect(Traits).toHaveBeenCalled();
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+	it.each([41, 42])("The TraitClass should be initialized on version %s", major => {
+		jest.isolateModules(() => {
+			jest.doMock("@shared/utils", () => ({
+				getVersion: () => ({
+					major,
+					minor: 20
+				})
+			}));
+			require("./Traits");
+		});
+
+		expect(Traits).toHaveBeenCalledTimes(major < 42 ? 1 : 0);
 	});
 });
